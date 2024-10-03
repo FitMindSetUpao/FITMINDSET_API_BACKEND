2fbcce7529a23373ab24cfc46cc5ce6da4a606a0
Sebastian Alonso Saavedra Arroyo <165932627+SaavedraSebastian@users.noreply.github.com>
2024-10-02 22:48:25 -0500

    Update UsuarioService.java

--- src/main/java/Grupo05/FitMindSet/Service/UsuarioService.java
+++ src/main/java/Grupo05/FitMindSet/Service/UsuarioService.java
@@ -1,4 +1,23 @@
-public interface UsuarioService{
+package Grupo05.FitMindSet.Service;
 
-  
+import Grupo05.FitMindSet.Exception.RoleNotFoundException;
+import Grupo05.FitMindSet.dto.AuthResponseDTO;
+import Grupo05.FitMindSet.dto.LoginDTO;
+import Grupo05.FitMindSet.dto.UserProfileDTO;
+import Grupo05.FitMindSet.dto.UserRegistrationDTO;
+
+public interface UsuarioService {
+    UserProfileDTO registerCustomer(UserRegistrationDTO registrationDTO) throws RoleNotFoundException;
+
+    UserProfileDTO registerAuthor(UserRegistrationDTO registrationDTO) throws RoleNotFoundException;
+
+    AuthResponseDTO login(LoginDTO loginDTO);
+
+    UserProfileDTO updateUserProfile(Long id, UserProfileDTO userProfileDTO);
+
+    UserProfileDTO getUserProfileById(Long id);
+
+    boolean isEmailRegistered(String correo);
+    
+    RegistroActividadResponseDTO registrarActividad(RegistroActividadRequestDTO request); // Método agregado a la interfaz
 }
