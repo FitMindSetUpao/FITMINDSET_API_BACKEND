ecfb362bb083e711b370a1103969ad8cae030ef7
Alexander Prieto <aprietom1@upao.edu.pe>
2024-10-02 21:59:28 -0500

    Creando controller

--- src/main/java/Grupo05/FitMindSet/Repository/UsuarioRepository.java
+++ src/main/java/Grupo05/FitMindSet/Repository/UsuarioRepository.java
@@ -1,10 +1,13 @@
 package Grupo05.FitMindSet.Repository;
 
 import Grupo05.FitMindSet.domain.Entity.Customer;
+import Grupo05.FitMindSet.domain.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Customer, Long> {
-
+    Optional<Usuario> findByCorreo(String correo);
 }
