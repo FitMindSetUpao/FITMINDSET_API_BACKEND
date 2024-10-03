b44852a006ab42870e2cdc5662af4486232fb185
Diogho Matteo Baylon Toledo <dbaylont1@upao.edu.pe>
2024-10-02 22:18:46 -0500

    Creando el UsuarioResponseDTO

--- src/main/java/Grupo05/FitMindSet/Repository/UsuarioRepository.java
+++ src/main/java/Grupo05/FitMindSet/Repository/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package Grupo05.FitMindSet.Repository;
 
-import Grupo05.FitMindSet.domain.Entity.Customer;
 import Grupo05.FitMindSet.domain.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/Grupo05/FitMindSet/Service/UsuarioService.java
+++ src/main/java/Grupo05/FitMindSet/Service/UsuarioService.java
@@ -1,11 +1,14 @@
 
 package Grupo05.FitMindSet.Service;
 
+import Grupo05.FitMindSet.Exception.UserNotFoundException;
 import Grupo05.FitMindSet.domain.Entity.Usuario;
 import Grupo05.FitMindSet.Repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import Grupo05.FitMindSet.dto.response.UsuarioResponseDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class UsuarioService {
 
