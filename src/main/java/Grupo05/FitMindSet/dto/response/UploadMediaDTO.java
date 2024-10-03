65723efa259db56306bbd4469361feb196b852a4
Diogho Matteo Baylon Toledo <dbaylont1@upao.edu.pe>
2024-10-02 23:23:21 -0500

    Creacion de MediaUploadDTO

---
+++ src/main/java/Grupo05/FitMindSet/dto/response/UploadMediaDTO.java
@@ -0,0 +1,10 @@
+package Grupo05.FitMindSet.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UploadMediaDTO {
+    private String path;
+}
