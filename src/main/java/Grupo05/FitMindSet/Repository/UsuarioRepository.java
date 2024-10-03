88e53e02e2e41ce3bd0f9ecd83b2c42c34532fcd
Diogho Matteo Baylon Toledo <dbaylont1@upao.edu.pe>
2024-10-02 22:17:40 -0500

    Creando el UsuarioResponseDTO

--- src/main/java/Grupo05/FitMindSet/Repository/UsuarioRepository.java
+++ src/main/java/Grupo05/FitMindSet/Repository/UsuarioRepository.java
@@ -1,10 +1,13 @@
 package Grupo05.FitMindSet.Repository;
 
 import Grupo05.FitMindSet.domain.Entity.Customer;
+import Grupo05.FitMindSet.domain.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UsuarioRepository extends JpaRepository<Customer, Long> {
+import java.util.Optional;
 
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findById(Long id);
 }
